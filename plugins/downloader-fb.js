86cbafbb777cf98157d5dd5e0464674606b67805
suppabro <130851096+suppabro@users.noreply.github.com>
2023-05-24 07:58:51 +0530

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -19,6 +19,7 @@ handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
 handler.premium = false;
+handler.group = true;
 handler.owner = false;
 handler.admin = false;
 handler.botAdmin = false;
