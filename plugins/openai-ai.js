08204c339319ee802d1d6c8f33eac8e8dbcfa08a
suppabro <130851096+suppabro@users.noreply.github.com>
2023-05-29 15:43:03 +0530

    Update openai-ai.js

--- plugins/openai-ai.js
+++ plugins/openai-ai.js
@@ -1,17 +1,31 @@
 var fetch = require('node-fetch');
+
 var handler = async (m, {
+
  text, 
+
  usedPrefix, 
+
  command
+
  }) => {
- const perintah = "Botkingdom is Best"
-if (!text) throw `උදාහරන!\n\n*Contoh:* Frist come egg or chicken? `
+
+if (!text) throw `Send any question 😒 `
+
 await m.reply(wait)
-  var apii = await fetch(`https://botcahx.cyclic.app/openai?logic=${perintah}&prompt=${text}`)
+
+  var apii = await fetch(`https://botcahx.cyclic.app/openai?text=${text}`)
+
   var js = await apii.json()
-  await m.reply(js.hasil)
+
+  await m.reply(js.result)
+
 }      
+
 handler.command = handler.help = ['ai','openai','chatgpt'];
-handler.tags = ['internet'];
+
+handler.tags = ['info'];
+
 handler.premium = false
+
 module.exports = handler;
